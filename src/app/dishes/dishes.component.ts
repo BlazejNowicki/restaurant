9bd6fcd3864294b0ddaa5c2de02ad9611f648ee7
Blazej Nowicki <blazej.a.now@gmail.com>
2022-01-02 19:40:02 +0100

    bez backendu

--- src/app/dishes/dishes.component.ts
+++ src/app/dishes/dishes.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Currency, DishTemplate } from '../app.component';
 import { DishManagmentService } from '../dish-managment.service';
-import { ItemComponent } from '../item/item.component';
 
 @Component({
   selector: 'app-dishes',
@@ -11,14 +10,16 @@ import { ItemComponent } from '../item/item.component';
 })
 export class DishesComponent implements OnInit {
   dishes: DishTemplate[] = [];
+  selection: DishTemplate[] = [];
   currency: Currency = Currency.Dolar;
 
   most_expensive_price: number = 0;
   cheapest_price: number = 0;
 
-  // page_size: number;
-  // page: number;
-  // sizes: number[];
+  page_size: number = 5;
+  page: number = 0;
+  sizes: number[] = [5,10,15];
+  show_next: boolean = false;
 
   constructor(private dish_managment: DishManagmentService) {
     this.dishes = dish_managment.getMenu();
@@ -27,6 +28,8 @@ export class DishesComponent implements OnInit {
       this.dishes = new_menu;
       this.find_most_least_expensive();
     });
+
+    this.pageChanged();
   }
 
   find_most_least_expensive() {
@@ -45,7 +48,26 @@ export class DishesComponent implements OnInit {
     this.find_most_least_expensive();
   }
 
+  prev(){
+    this.page -= 1;
+    this.pageChanged();
+  }
+
+  next(){
+    this.page += 1;
+    this.pageChanged();
+  }
+
+  pageChanged(){
+    this.show_next = (this.page + 1) * this.page_size < this.dishes.length;
+    this.selection = this.dishes.slice(this.page * this.page_size, (this.page + 1) * this.page_size);
+  }
+
   onPageSizeChange(deviceValue: any) {
     console.log(deviceValue);
+    this.page_size = parseInt(deviceValue.target.value);
+    console.log(this.page_size);
+    this.page = 0;
+    this.pageChanged();
   }
 }
